fec479347afc791c06abfcb6559beb8c85f62908
Paul Morris <pmorris2012@fau.edu>
2020-10-05 06:04:57 -0400

    coordinate rescaling to maintain aspect ratio

--- examples/python/process_folder.py
+++ examples/python/process_folder.py
@@ -65,28 +65,20 @@ def draw_pose(image, result, modes):
         image = draw_keypoints(image, mode=mode)
     return image
 
-def save_coords(path, result, modes):
+def rescale_coord_array(array, frame_size):
+    rescale_dim = np.argmin(frame_size)
+    pixel_offset = (max(frame_size) - min(frame_size)) / 2
+    array[:,:,rescale_dim] += pixel_offset
+    array[:,:,:2] /= max(frame_size)
+    return array
+
+def rescale_coords(result, modes, frame_size):
     arrays = {}
     for mode in modes:
         array = array_dict[mode](result)
         if len(array.shape) > 0:
-            arrays[mode] = array
-    
-    np.savez(path, **arrays)
-
-#os.walk recursively goes through all the files in args.input_folder
-for directory, folders, files in os.walk(args.input_folder):
-    print(F"searching {directory}")
-    image_paths, video_paths = find_images_videos(directory, files)
-    print(F"found {len(image_paths)} images and {len(video_paths)} videos")
-    
-    if len(image_paths) > 0:
-        for image_path in tqdm(image_paths, desc='images'):
-            process_image(image_path)
-
-    if len(video_paths) > 0:
-        for video_path in tqdm(video_paths, desc='videos'):
-            process_video(video_path)
+            arrays[mode] = rescale_coord_array(array, frame_size)
+    return arrays
 
 def process_image(image_path):
     pass
@@ -112,15 +104,16 @@ def process_video(video_path):
         result = get_keypoints(frame)
 
         if args.draw_pose:
-            image_pose = draw_pose(frame, result)
+            image_pose = draw_pose(frame, result, modes)
             video_pose.write(image_pose)
         if args.draw_black_pose:
             black = np.zeros_like(frame) #get black image
-            black_pose = draw_pose(black, result)
+            black_pose = draw_pose(black, result, modes)
             video_black_pose.write(black_pose)
 
         coords_frame_path = os.path.join(coords_path, str(frame_idx) + ".npz")
-        save_coords(coords_frame_path, result)
+        coord_arrays = rescale_coords(result, modes)
+        np.savez(coords_frame_path, **coord_arrays)
 
         frames_remaining, frame = video.read()
         frame_idx += 1
@@ -132,3 +125,17 @@ def process_video(video_path):
         video_pose.release()
     if args.draw_black_pose:
         video_black_pose.release()
+
+#os.walk recursively goes through all the files in args.input_folder
+for directory, folders, files in os.walk(args.input_folder):
+    print(F"searching {directory}")
+    image_paths, video_paths = find_images_videos(directory, files)
+    print(F"found {len(image_paths)} images and {len(video_paths)} videos")
+    
+    if len(image_paths) > 0:
+        for image_path in tqdm(image_paths, desc='images'):
+            process_image(image_path)
+
+    if len(video_paths) > 0:
+        for video_path in tqdm(video_paths, desc='videos'):
+            process_video(video_path)
