fdfb078b17831108fa4d290155e3fecb8fc586aa
Paul Morris <pmorris2012@fau.edu>
2020-10-05 09:03:08 -0400

    merged python scripts into docker image

--- docker_container_run.sh
+++ docker_container_run.sh
@@ -5,9 +5,8 @@ docker run \
     --rm \
     -v /media/mpcrpaul/data/HMDB51/dataset:/Input \
     -v /media/mpcrpaul/data/HMDB51/Videos_Pose:/Output \
-    -v /home:/home \
     --gpus all \
     --ipc="host" \
     pmorris2012/openpose \
-    python3 /home/mpcrpaul/Documents/school/openpose/examples/python/process_folder.py \
+    python3 /openpose/examples/python/process_folder.py \
     --draw_pose --draw_black_pose --scale_number 4
