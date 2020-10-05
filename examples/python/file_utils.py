8bc5034f3b9938966661deee1580715db0d67ffe
Paul Morris <pmorris2012@fau.edu>
2020-10-05 05:46:15 -0400

    video loop refactor complete

--- examples/python/file_utils.py
+++ examples/python/file_utils.py
@@ -4,12 +4,15 @@ def move_path(path, folder_from, folder_to):
     return path.replace(folder_from, folder_to, 1)
 
 def replace_ext(file, ext):
-    if not ext.startswith('.'):
-        ext = '.' + ext
     return os.path.splitext(file)[0] + ext
 
 def create_dirs(path):
     directory = os.path.dirname(path)
     if not os.path.exists(directory):
         os.makedirs(directory)
-        
+
+def create_write_dir(path, base_dir, ext):
+    write_path = move_path(path, args.input_folder, base_dir)
+    write_path = replace_ext(write_path, ext)
+    create_dirs(write_path)
+    return write_path
--- examples/python/process_folder.py
+++ examples/python/process_folder.py
@@ -7,7 +7,7 @@ import argparse
 from tqdm import tqdm
 
 from drawing import draw_keypoints
-from file_utils import move_path, replace_ext, create_dirs
+from file_utils import create_write_dir
 from cv_tuils import find_images_videos, get_video_properties
 
 parser = argparse.ArgumentParser()
@@ -40,12 +40,6 @@ opWrapper = op.WrapperPython()
 opWrapper.configure(params)
 opWrapper.start()
 
-def get_keypoints(image):
-    datum = op.Datum()
-    datum.cvInputData = image
-    opWrapper.emplaceAndPop([datum])
-    return datum
-
 modes = ['body']
 if args.face: 
     modes.append('face')
@@ -58,6 +52,12 @@ array_dict = {
     "handl": lambda result: result.handKeypoints[0],
     "handr": lambda result: result.handKeypoints[1]
 }
+
+def get_keypoints(opWrapper, image):
+    datum = op.Datum()
+    datum.cvInputData = image
+    opWrapper.emplaceAndPop([datum])
+    return datum
 
 def draw_pose(image, result, modes):
     for mode in modes:
@@ -74,7 +74,7 @@ def save_coords(path, result, modes):
     
     np.savez(path, **arrays)
 
-#os.walk recursively goes through all the files in our args.input_folder
+#os.walk recursively goes through all the files in args.input_folder
 for directory, folders, files in os.walk(args.input_folder):
     print(F"searching {directory}")
     image_paths, video_paths = find_images_videos(directory, files)
@@ -94,51 +94,41 @@ def process_image(image_path):
 def process_video(video_path):
     video = cv2.VideoCapture(video_path)
     props = get_video_properties(video)
+    frame_size = (props['width'], props['height'])
 
     if args.draw_pose:
-        pose_path = move_path(video_path, args.input_folder, pose_dir)
-        pose_path = replace_ext(pose_path, args.video_ext)
-        create_dirs(pose_path)
-        video_pose = cv2.VideoWriter(pose_path, FOURCC_CODE, props['fps'], (props['width'], props['height']))
-
+        pose_path = create_write_dir(video_path, pose_dir, ext=args.video_ext)
+        video_pose = cv2.VideoWriter(pose_path, FOURCC_CODE, props['fps'], frame_size)
     if args.draw_black_pose:
-        black_pose_path = move_path(input_path, args.input_folder, black_pose_dir)
-        black_pose_path = replace_ext(black_pose_path, args.video_ext)
-        create_dirs(black_pose_path)
-        video_black_pose = cv2.VideoWriter(black_pose_path, FOURCC_CODE, props['fps'], (props['width'], props['height']))
+        black_pose_path = create_write_dir(video_path, black_pose_dir, ext=args.video_ext)
+        video_black_pose = video_pose = cv2.VideoWriter(black_pose_path, FOURCC_CODE, props['fps'], frame_size)
+        
+    coords_path = create_write_dir(video_path, coords_dir, ext='/')
     
-    coords_path = move_path(input_path, args.input_folder, coords_dir)
-    coords_path, _ext = os.path.splitext(coords_path)
-    create_dirs(os.path.join(coords_path, ""))
-
-for input_path in input_paths:
-    video = cv2.VideoCapture(input_path)
-    
-    
-    
-    
-    
-    
+    progress_bar = tqdm(initial=1, total=props['frames'], desc=os.path.basename(video_path)[:15])
     frames_remaining, frame = video.read()
     frame_idx = 0
     while frames_remaining:
         result = get_keypoints(frame)
 
-        image_pose = draw_pose(frame, result)
-        video_pose.write(image_pose)
-
-        black = np.zeros_like(frame) #get black image
-        black_pose = draw_pose(black, result)
-        video_black_pose.write(black_pose)
+        if args.draw_pose:
+            image_pose = draw_pose(frame, result)
+            video_pose.write(image_pose)
+        if args.draw_black_pose:
+            black = np.zeros_like(frame) #get black image
+            black_pose = draw_pose(black, result)
+            video_black_pose.write(black_pose)
 
         coords_frame_path = os.path.join(coords_path, str(frame_idx) + ".npz")
         save_coords(coords_frame_path, result)
 
-        log_video_progress(video)
         frames_remaining, frame = video.read()
         frame_idx += 1
+        progress_bar.update(1)
         
-    print(input_path)
+    progress_bar.close()
     video.release()
-    video_pose.release()
-    video_black_pose.release()
+    if args.draw_pose:
+        video_pose.release()
+    if args.draw_black_pose:
+        video_black_pose.release()
