c58cb0c9982dea2b824958bdc09d94e6ee34448d
Paul Morris <pmorris2012@fau.edu>
2020-10-06 16:57:35 -0400

    added container run scripts

--- docker_container_run.sh
+++ docker_container_run.sh
@@ -3,10 +3,7 @@
 docker run \
     -it \
     --rm \
-    -v /media/mpcrpaul/data/HMDB51/dataset:/Input \
-    -v /media/mpcrpaul/data/HMDB51/Videos_Pose:/Output \
+    -v /home:/home \
     --gpus all \
     --ipc="host" \
-    pmorris2012/openpose \
-    python3 /openpose/examples/python/process_folder.py \
-    --draw_pose --draw_black_pose --scale_number 4
+    pmorris2012/openpose
---
+++ examples/docker_scripts/run_coords+pose+blackpose_body+face+hands_defaultsettings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker run \
+    -it \
+    --rm \
+    -v /media/mpcrpaul/data/HMDB51/dataset:/Input \
+    -v /media/mpcrpaul/data/HMDB51/Videos_Pose_Face_Hands:/Output \
+    --gpus all \
+    --ipc="host" \
+    pmorris2012/openpose \
+    python3 /openpose/examples/python/process_folder.py \
+    --draw_pose --draw_black_pose --face --hand
