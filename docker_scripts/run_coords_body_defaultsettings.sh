16764ddd4208e66565f064fee1dda24c8b18accf
Paul Morris <pmorris2012@fau.edu>
2020-10-06 18:33:09 -0400

    refer to relative directory

--- docker_scripts/run_coords_body_defaultsettings.sh
+++ docker_scripts/run_coords_body_defaultsettings.sh
@@ -5,8 +5,8 @@ docker run \
     --rm \
     -v /media/mpcrpaul/data/HMDB51/dataset:/Input \
     -v /media/mpcrpaul/data/HMDB51/Videos_Pose:/Output \
-    -v /home/mpcrpaul/Documents/school/openpose/examples/python:/code \
+    -v $(dirname $(pwd))/examples/python:/code \
     --gpus all \
     --ipc="host" \
     pmorris2012/openpose \
-    python3 /code/process_folder.py
+    python3 /code/process_folder.py --help
